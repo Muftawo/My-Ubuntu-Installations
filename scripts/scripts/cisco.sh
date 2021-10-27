b79390df4426cea4a6ff823c905617f48d74e52c
Muftawo <51520018+Muftawo@users.noreply.github.com>
2021-10-27 16:45:18 +0000

    Add files via upload

---
+++ scripts/scripts/cisco.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "-----------------------------"
+echo "Updating Ubuntu files"
+echo "-----------------------------"
+sudo apt update
+
+echo "-----------------------------"
+echo "Installing "
+echo "-----------------------------"
+
+sudo apt-get install network-manager-vpnc-gnome
+echo "-----------------------------"
+echo "done"
+
